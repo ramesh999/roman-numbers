c90399e1e3b29026ffece7ed5b5851fd5602dbcf
Ramesh <kramesh@Rameshs-MacBook-Pro.local>
2022-03-08 06:28:49 +0000

    implemenation not to allow negative numbers

--- src/main/java/com/il/RomanNumbers.java
+++ src/main/java/com/il/RomanNumbers.java
@@ -2,6 +2,9 @@ package com.il;
 
 public class RomanNumbers {
     public String toRomanNumber(int num){
+        if(num < 0){
+            throw new IllegalArgumentException("Number should be greater than zero");
+        }
         return "";
     }
 }
