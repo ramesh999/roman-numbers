1d73cdd4750003beb894efa79e571c2f8ebfd153
Ramesh <kramesh@Rameshs-MacBook-Pro.local>
2022-03-08 08:07:16 +0000

    refacotored to use TreeMap

--- src/main/java/com/il/RomanNumbers.java
+++ src/main/java/com/il/RomanNumbers.java
@@ -1,68 +1,45 @@
 package com.il;
 
+import java.util.TreeMap;
+
+import static java.util.Objects.nonNull;
+
 public class RomanNumbers {
 
     public static final String ILLEAGAL_EXCEPTION_MESSAGE =
             "Number should be greater than zero and less than or equal to 3000";
+    private final TreeMap<Integer, String> map;
+
+    public RomanNumbers() {
+        map = new TreeMap<>();
+        map.put(1000, "M");
+        map.put(900, "CM");
+        map.put(500, "D");
+        map.put(400, "CD");
+        map.put(100, "C");
+        map.put(90, "XC");
+        map.put(50, "L");
+        map.put(40, "XL");
+        map.put(10, "X");
+        map.put(9, "IX");
+        map.put(5, "V");
+        map.put(4, "IV");
+        map.put(1, "I");
+    }
 
     public String toRomanNumber(int num) {
+
         if (num <= 0 || num > 3000) {
             throw new IllegalArgumentException(ILLEAGAL_EXCEPTION_MESSAGE);
         }
 
         int temp = num;
+        Integer key = map.floorKey(num);
         String value = "";
-        while (temp >= 1000){
-            value += "M";
-            temp = temp - 1000;
-        }
-        while (temp >= 900){
-            value += "CM";
-            temp = temp - 900;
-        }
-        while (temp >= 500){
-            value += "D";
-            temp = temp - 500;
-        }
-        while (temp >= 400){
-            value += "CD";
-            temp = temp - 400;
-        }
-        while (temp >= 100){
-            value += "C";
-            temp = temp - 100;
-        }
-        while (temp >= 90){
-            value += "XC";
-            temp = temp - 90;
-        }
-        while (temp >= 50){
-            value += "L";
-            temp = temp - 50;
-        }
-        while (temp >= 40){
-            value += "XL";
-            temp = temp - 40;
-        }
-        while (temp >= 10) {
-            value += "X";
-            temp = temp - 10;
-        }
-        while (temp >= 9) {
-            value += "IX";
-            temp = temp - 9;
-        }
-        while (temp >= 5) {
-            value += "V";
-            temp = temp - 5;
-        }
-        while (temp >= 4) {
-            value += "IV";
-            temp = temp - 4;
-        }
-        while (temp >= 1) {
-            value += "I";
-            temp = temp - 1;
+        while (nonNull(key)) {
+            value += map.get(key);
+            temp = temp - key;
+            key = map.floorKey(temp);
         }
         return value;
     }
