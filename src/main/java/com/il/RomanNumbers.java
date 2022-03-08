7d736424e0f4db7049d31ebce13232d1d984e016
Ramesh <kramesh@Rameshs-MacBook-Pro.local>
2022-03-08 06:38:52 +0000

    Invalid argument test  with implemenation

--- src/main/java/com/il/RomanNumbers.java
+++ src/main/java/com/il/RomanNumbers.java
@@ -1,9 +1,13 @@
 package com.il;
 
 public class RomanNumbers {
-    public String toRomanNumber(int num){
-        if(num < 0){
-            throw new IllegalArgumentException("Number should be greater than zero");
+
+    public static final String ILLEAGAL_EXCEPTION_MESSAGE =
+            "Number should be greater than zero and less than or equal to 3000";
+
+    public String toRomanNumber(int num) {
+        if (num <= 0 || num > 3000) {
+            throw new IllegalArgumentException(ILLEAGAL_EXCEPTION_MESSAGE);
         }
         return "";
     }
--- src/test/java/com/il/RomanNumbersTest.java
+++ src/test/java/com/il/RomanNumbersTest.java
@@ -2,7 +2,9 @@ package com.il;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static com.il.RomanNumbers.ILLEAGAL_EXCEPTION_MESSAGE;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class RomanNumbersTest {
 
@@ -18,8 +20,37 @@ class RomanNumbersTest {
                         "Should throw exception");
 
         // then
-        assertEquals("Number should be greater than zero", actual.getMessage(), "Message should be equal.");
+        assertEquals(ILLEAGAL_EXCEPTION_MESSAGE, actual.getMessage(), "Message should be equal.");
     }
 
+    @Test
+    void toRomanNumber_shouldThrowInvalidArgumentException_whenZeroPassed() {
+        // given
+        RomanNumbers romanNumbers = new RomanNumbers();
+        // when
+        IllegalArgumentException actual =
+                assertThrows(
+                        IllegalArgumentException.class,
+                        () -> romanNumbers.toRomanNumber(0),
+                        "Should throw exception");
+
+        // then
+        assertEquals(ILLEAGAL_EXCEPTION_MESSAGE, actual.getMessage(), "Message should be equal.");
+    }
+
+    @Test
+    void toRomanNumber_shouldThrowInvalidArgumentException_whenNumberGreaterThan3000Passed() {
+        // given
+        RomanNumbers romanNumbers = new RomanNumbers();
+        // when
+        IllegalArgumentException actual =
+                assertThrows(
+                        IllegalArgumentException.class,
+                        () -> romanNumbers.toRomanNumber(3001),
+                        "Should throw exception");
+
+        // then
+        assertEquals(ILLEAGAL_EXCEPTION_MESSAGE, actual.getMessage(), "Message should be equal.");
+    }
 
 }
