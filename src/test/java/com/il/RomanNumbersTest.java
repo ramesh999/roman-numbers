68a8dbe61120d623f81ab456deeb2d7fcba5dd4b
Ramesh <kramesh@Rameshs-MacBook-Pro.local>
2022-03-08 06:25:50 +0000

    Illegal Arguments : negative numbers

--- src/test/java/com/il/RomanNumbersTest.java
+++ src/test/java/com/il/RomanNumbersTest.java
@@ -1,7 +1,25 @@
 package com.il;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RomanNumbersTest {
+
+    @Test
+    void toRomanNumber_shouldThrowInvalidArgumentException_whenNegativeNumbersPassed() {
+        // given
+        RomanNumbers romanNumbers = new RomanNumbers();
+        // when
+        IllegalArgumentException actual =
+                assertThrows(
+                        IllegalArgumentException.class,
+                        () -> romanNumbers.toRomanNumber(-1),
+                        "Should throw exception");
+
+        // then
+        assertEquals("Number should be greater than zero", actual.getMessage(), "Message should be equal.");
+    }
+
 
 }
